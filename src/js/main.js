7f28983fec6a0dd1dc705f4fc2c2af77ff83f961
Ilya758 <subscriber6436@gmail.com>
2021-08-11 21:24:50 +0300

    feat: add modal windows - popups and repair style.scss + main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(() => {
+  const formWrapper = document.querySelector('.wrapper_elem_form');
+  const popupWrapper = document.querySelector('.wrapper_elem_popup');
+  const thanksWrapper = document.querySelector('.wrapper_elem_popup-thanks');
+  const closeButton = document.querySelectorAll('.wrapper_elem_close-btn');
+
+  const classHandler = (elem, cls, method) => {
+    if (method === 'add') {
+      elem.classList.add(cls);
+    } else {
+      elem.classList.remove(cls);
+    }
+  };
+
   $('.rooms__slider').slick({
     dots: true,
     slidesToShow: 1,
@@ -22,5 +35,69 @@ $(document).ready(() => {
         centerMode: true,
       }
     }],
+  });
+
+  const formButton = document.querySelector('#btn');
+  const popupButton = document.querySelector('.btn_c_white');
+  const roomsButton = document.querySelectorAll('.rooms__btn');
+  const galleryButton = document.querySelector('.gallery__btn');
+
+  const addListener = (cls, btns, type) => {
+
+    if (type === 'NodeList') {
+      if (Object.values(btns).length === 0) {
+        return;
+      }
+    } else {
+      if (btns === null) {
+        return;
+      }
+    }
+
+    if (Object.values(btns).length > 1) {
+      for (let btn of btns) {
+        btn.addEventListener('click', () => {
+          classHandler(cls, 'active_anim', 'add');
+        });
+      }
+    } else {
+      btns.addEventListener('click', () => {
+        classHandler(cls, 'active_anim', 'add');
+      });
+    }
+  };
+
+  addListener(formWrapper, formButton);
+  addListener(popupWrapper, popupButton);
+  addListener(popupWrapper, roomsButton, 'NodeList');
+  addListener(popupWrapper, galleryButton);
+
+  closeButton.forEach((b) => { // smoothly removes classes when generate the event
+    b.addEventListener('click', () => {
+      let sort = [];
+      [formWrapper, popupWrapper, thanksWrapper].forEach((wrapper) => {
+        if (wrapper !== null) {
+          sort.push(wrapper);
+        }
+      });
+
+      sort.forEach((wrap) => {
+        if (wrap === formWrapper || document.documentElement.clientWidth > 1200) {
+          wrap.style.cssText = 'transition: 0.5s ease-in; opacity: 0';
+          wrap.style.opacity = '1';
+        } else {
+          wrap.style.cssText = 'transition: 0.5s ease-in; opacity: 0';
+        }
+      });
+
+      sort.forEach((wrap) => {
+        setTimeout(() => {
+          wrap.classList.remove('active', 'active_anim');
+          if (wrap !== formWrapper && document.documentElement.clientWidth < 1200) {
+            wrap.style.cssText = 'display: none';
+          }
+        }, 510);
+      });
+    });
   });
 });
