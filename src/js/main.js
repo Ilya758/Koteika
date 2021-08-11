5b0385d87c566501881ca97f0ce595d1f41499da
Ilya758 <subscriber6436@gmail.com>
2021-08-11 21:14:43 +0300

    feat: add section reviews with adaptive slider

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,19 @@ $(document).ready(() => {
     appendDots: '.rooms__controls-dots',
     appendArrows: '.rooms__controls-arrows',
   });
+
+  $('.reviews__slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    variableWidth: true,
+    appendDots: '.reviews__controls-dots',
+    appendArrows: '.reviews__controls-arrows',
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+      }
+    }],
+  });
 });
